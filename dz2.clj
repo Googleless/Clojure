9b8231b5186e28aea82dc4465ef3cf90e02280e4
Googleless <filipp.zemlyanskiy@mail.ru>
2025-06-04 12:26:46 +0300

    major reformatting changes
    + added new tasks

--- dz2.clj
+++
@@ -1,9 +0,0 @@
-(defn double-elements [coll]
-  (map #(* % 2) coll))
-
-(println (double-elements [1 2 3])) ;; (2 4 6)
-
-(defn sum-elements [coll]
-  (reduce + coll))
-
-(println (sum-elements [1 2 3])) ;; 6
